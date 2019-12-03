312f1072e646a58808bd281ccbf026696013ae66
Luoyl233 <luoyili233@gmail.com>
2019-12-03 22:49:50 +0800

    Optimize log compaction

--- test.sh
+++ test.sh
@@ -11,15 +11,15 @@ do
 #go test -run Election && \
 
 # Lab 2 part 1 tests
-#go test -run FailNoAgree && \
-#go test -run ConcurrentStarts && \
-#go test -run Rejoin && \
-#go test -run Backup && \
+go test -run FailNoAgree && \
+go test -run ConcurrentStarts && \
+go test -run Rejoin && \
+go test -run Backup && \
 
 # Lab 2 part 2 tests
-go test -run Persist1 && \
-go test -run Persist2 && \
-go test -run Persist3 && \
+#go test -run Persist1 && \
+#go test -run Persist2 && \
+#go test -run Persist3 && \
 
 #echo "Passed "${i}
 N=$[N+1]
