7050bfcc5ca6392f03093b3ea768ddc7883803c5
Luoyl233 <luoyili233@gmail.com>
2019-12-04 01:16:38 +0800

    Fix Leader在RV中转换为Follower未设置electionTimer

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ for ((i=1;i<=${T};i++))
 do
 
 # Lab 1 tests
-#go test -run Election && \
+go test -run Election && \
 
 # Lab 2 part 1 tests
 go test -run FailNoAgree && \
@@ -17,9 +17,9 @@ go test -run Rejoin && \
 go test -run Backup && \
 
 # Lab 2 part 2 tests
-#go test -run Persist1 && \
-#go test -run Persist2 && \
-#go test -run Persist3 && \
+go test -run Persist1 && \
+go test -run Persist2 && \
+go test -run Persist3 && \
 
 #echo "Passed "${i}
 N=$[N+1]
